0e36cc7369db4215c3b114a04e9dc040277825ea
chengshuang18 <chengs18@qq.com>
2024-10-07 23:44:37 +0800

    initial

--- scripts/standard_benchmark_ft.sh
+++ scripts/standard_benchmark_ft.sh
@@ -16,21 +16,21 @@ for ini_threshold in "${ini_thresholds[@]}"; do
         echo "Log file already exists: $LOGFILE"
     fi
 
-    # mkdir -p output/${model}/${tuning_method}/${method}/order_2/logs/${seed}
-    # LOGFILE="output/${model}/${tuning_method}/${method}/order_2/logs/${seed}/train_and_infer_${ini_threshold}.log"
-    # if [ ! -f "$LOGFILE" ]; then
-    #     bash scripts/full_tuning/order_2.sh ${model} ${tuning_method} ${method} ${ini_threshold} ${seed} > "$LOGFILE" 2>&1
-    # else
-    #     echo "Log file already exists: $LOGFILE"
-    # fi
+    mkdir -p output/${model}/${tuning_method}/${method}/order_2/logs/${seed}
+    LOGFILE="output/${model}/${tuning_method}/${method}/order_2/logs/${seed}/train_and_infer_${ini_threshold}.log"
+    if [ ! -f "$LOGFILE" ]; then
+        bash scripts/full_tuning/order_2.sh ${model} ${tuning_method} ${method} ${ini_threshold} ${seed} > "$LOGFILE" 2>&1
+    else
+        echo "Log file already exists: $LOGFILE"
+    fi
 
-    # mkdir -p output/${model}/${tuning_method}/${method}/order_3/logs/${seed}
-    # LOGFILE="output/${model}/${tuning_method}/${method}/order_3/logs/${seed}/train_and_infer_${ini_threshold}.log"
-    # if [ ! -f "$LOGFILE" ]; then
-    #     bash scripts/full_tuning/order_3.sh ${model} ${tuning_method} ${method} ${ini_threshold} ${seed} > "$LOGFILE" 2>&1
-    # else
-    #     echo "Log file already exists: $LOGFILE"
-    # fi
+    mkdir -p output/${model}/${tuning_method}/${method}/order_3/logs/${seed}
+    LOGFILE="output/${model}/${tuning_method}/${method}/order_3/logs/${seed}/train_and_infer_${ini_threshold}.log"
+    if [ ! -f "$LOGFILE" ]; then
+        bash scripts/full_tuning/order_3.sh ${model} ${tuning_method} ${method} ${ini_threshold} ${seed} > "$LOGFILE" 2>&1
+    else
+        echo "Log file already exists: $LOGFILE"
+    fi
 
   done
 done
